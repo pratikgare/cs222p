6471576b1acc923d5c4aa31196e83886ad8bc0a8
RAJAT GOYAL <rajatgoyal@RAJATs-MacBook-Pro.local>
2017-11-26 13:28:02 -0800

    inserting private test cases too

---
+++ src/rbf/test.sh
@@ -0,0 +1,26 @@
+
+make clean
+make
+./rbftest1
+./rbftest2
+./rbftest3
+./rbftest4
+./rbftest5
+./rbftest6
+./rbftest7
+./rbftest8
+./rbftest9
+./rbftest10
+./rbftest11
+./rbftest12
+./rbftest_p0
+./rbftest_p1
+./rbftest_p1b
+./rbftest_p1c
+./rbftest_p2
+./rbftest_p2b
+./rbftest_p3
+./rbftest_p4
+./rbftest_p5
+./rbftest_delete
+./rbftest_update
